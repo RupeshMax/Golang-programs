c0d41adffbd5732c6bedbce16cff2182920273c2
RupeshMax <rupesh.maxpain@gmail.com>
2020-06-17 19:36:55 +0530

    program

---
+++ VariableAndDatatype.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var String string = "Hello World"
+	var UnsignedNumber uint8 = 100
+	var Number int8 = -100
+	var Float float32 = 0.2
+
+
+	fmt.Println(String, Number, UnsignedNumber, Float)
+	
+
+}
